6b0c43fa5b7e6809b797b4d82a695504fcab83e3
Woody-Xiong <87098248+Woody-Xiong@users.noreply.github.com>
2022-02-01 16:02:07 +0800

    Update Utils.java

--- src/test/java/com/example/restcountries/common/Utils.java
+++ src/test/java/com/example/restcountries/common/Utils.java
@@ -59,57 +59,57 @@ public class Utils {
         String nativeName = "Suomi";
         String numericCode = "246";
 
-//        List<Map<?,?>> currencies = new ArrayList<>();
-//        Map<String, String> currencies_1 = new HashMap<>();
-//        currencies_1.put("code", "EUR");
-//        currencies_1.put("name", "Euro");
-//        currencies_1.put("symbol", "€");
-//        currencies.add(currencies_1);
+        List<Map<?,?>> currencies = new ArrayList<>();
+        Map<String, String> currencies_1 = new HashMap<>();
+        currencies_1.put("code", "EUR");
+        currencies_1.put("name", "Euro");
+        currencies_1.put("symbol", "€");
+        currencies.add(currencies_1);
+////
 //
+        List<Map<?,?>> languages = new ArrayList<>();
+        Map<String, String> languages_1 = new HashMap<>();
+        languages_1.put("iso639_1", "fi");
+        languages_1.put("iso639_2", "fin");
+        languages_1.put("name", "Finnish");
+        languages_1.put("nativeName", "suomi");
+        languages.add(languages_1);
+////
+        Map<String, String> languages_2 = new HashMap<>();
+        languages_2.put("iso639_1", "sv");
+        languages_2.put("iso639_2", "swe");
+        languages_2.put("name", "Swedish");
+        languages_2.put("nativeName", "svenska");
+        languages.add(languages_2);
+        
+        Map<String, String> translations = new HashMap<>();
+        translations.put("br", "Finlândia");
+        translations.put("pt", "Finlândia");
+        translations.put("nl", "Finland");
+        translations.put("hr", "Finska");
+        translations.put("fa", "فنلاند");
+        translations.put("de", "Finnland");
+        translations.put("es", "Finlandia");
+        translations.put("fr", "Finlande");
+        translations.put("ja", "フィンランド");
+        translations.put("it", "Finlandia");
+        translations.put("hu", "Finnország");
+
+
+
+        Map<String, String> flags = new HashMap<>();
+        flags.put("svg", "https://flagcdn.com/fi.svg");
+        flags.put("png", "https://flagcdn.com/w320/fi.png");
 //
-//        List<Map<?,?>> languages = new ArrayList<>();
-//        Map<String, String> languages_1 = new HashMap<>();
-//        languages_1.put("iso639_1", "fi");
-//        languages_1.put("iso639_2", "fin");
-//        languages_1.put("name", "Finnish");
-//        languages_1.put("nativeName", "suomi");
-//        languages.add(languages_1);
-//
-//        Map<String, String> languages_2 = new HashMap<>();
-//        languages_2.put("iso639_1", "sv");
-//        languages_2.put("iso639_2", "swe");
-//        languages_2.put("name", "Swedish");
-//        languages_2.put("nativeName", "svenska");
-//        languages.add(languages_2);
-//
-//
-//        List<Map<?,?>> translations = new ArrayList<>();
-//        Map<String, String> translations_1 = new HashMap<>();
-//        translations_1.put("br", "Finlândia");
-//        translations_1.put("pt", "Finlândia");
-//        translations_1.put("nl", "Finland");
-//        translations_1.put("hr", "Finska");
-//        translations_1.put("fa", "فنلاند");
-//        translations_1.put("de", "Finnland");
-//        translations_1.put("es", "Finlandia");
-//        translations_1.put("fr", "Finlande");
-//        translations_1.put("ja", "フィンランド");
-//        translations_1.put("hu", "Finnország");
-//        translations.add(translations_1);
-//
-//
-//
-//        Map<String, String> flags = new HashMap<>();
-//        flags.put("svg", "https://flagcdn.com/fi.svg");
-//        flags.put("png", "https://flagcdn.com/w320/fi.png");
-//
-//        List<Map<?,?>> regionalBlocs = new ArrayList<>();
-//        Map<String, String> regionalBlocs_1 = new HashMap<>();
-//        regionalBlocs_1.put("acronym", "EU");
-//        regionalBlocs_1.put("name", "European Union");
-//        regionalBlocs.add(regionalBlocs_1);
+        List<Map<?,?>> regionalBlocs = new ArrayList<>();
+        Map<String, String> regionalBlocs_1 = new HashMap<>();
+        regionalBlocs_1.put("acronym", "EU");
+        regionalBlocs_1.put("name", "European Union");
+        regionalBlocs.add(regionalBlocs_1);
 
         String cioc = "FIN";
+
+        Boolean independent = true;
 
         if (entity){
             return Country.builder()
@@ -133,12 +133,13 @@ public class Utils {
                     .borders(borders)
                     .nativeName(nativeName)
                     .numericCode(numericCode)
-//                    .currencies(currencies)
-//                    .languages(languages)
-//                    .translations(translations)
-//                    .flags(flags)
-//                    .regionalBlocs(regionalBlocs)
+                    .currencies(currencies)
+                    .languages(languages)
+                    .translations(translations)
+                    .flags(flags)
+                    .regionalBlocs(regionalBlocs)
                     .cioc(cioc)
+                    .independent(independent)
                     .build();
         }else {
             return Country.builder().build();
@@ -146,5 +147,4 @@ public class Utils {
 
     }
 }
-
 
